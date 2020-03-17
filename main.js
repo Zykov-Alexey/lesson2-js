fc46a452af1b3acb5bd0611ff6febf20aee03ac2
Alexey Zykov <zykov.alexey@gmail.com>
2020-03-17 16:53:15 +0200

    Сделал 2е задание

---
+++ main.js
@@ -0,0 +1,11 @@
+var userObj = {
+  firstName: 'Alexandr',
+  lastName: 'Pushkin',
+  age: 25,
+  fullName: function(){
+  return fname = this.firstName + ' ' + this.lastName;
+    },
+};
+
+userObj.fullName();
+
