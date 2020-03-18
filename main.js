293ed967c9e64007251c8bc81c0f064fc5c92a30
Alexey Zykov <zykov.alexey@gmail.com>
2020-03-18 12:45:52 +0200

    Сделал 3е и 4 задание

--- main.js
+++ main.js
@@ -6,6 +6,27 @@ var userObj = {
   return fname = this.firstName + ' ' + this.lastName;
     },
 };
-
+console.log(userObj);
 userObj.fullName();
+
+// задача3
+
+function defUpperStr(mytext) {
+  return (mytext || 'default text').toUpperCase();
+}
+console.log(defUpperStr('My text'));
+console.log(defUpperStr()); 
+
+// задача4
+function evenFn(n) {
+  var arr = [];
+  for (var i = 2; i<=n; i+=2) arr.push(i);
+  return arr;
+}
+console.log(evenFn(10));
+console.log(evenFn(15));
+console.log(evenFn(20));
+
+
+
 
